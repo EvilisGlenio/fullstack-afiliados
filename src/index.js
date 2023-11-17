b07dd50c915f999ba912a0aedad33234c0f7870c
Evilis Glenio <glenio.developer@gmail.com>
2023-11-17 14:12:12 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json()); // so that the express starts giving you the body of messages coming in json format
+app.use(express.urlencoded({ extended: true})); // so that express can give you requests in the urlenconded pattern
+app.use(morgan('dev')); // log lib
 
-app.use(require("./routes"))
+app.use(require("./routes"));
 
 app.listen(3000);
