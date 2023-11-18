7721c1ce268ec4dd9cafe08a592c84ccb7289404
Evilis Glenio <glenio.developer@gmail.com>
2023-11-18 09:32:47 -0300

    amazon aws configuration

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,41 +1,47 @@
-const multer = require('multer');
-const path = require('path');
-const crypto = require('crypto');
+const multer = require("multer");
+const path = require("path");
+const crypto = require("crypto");
+const aws = require("aws-sdk");
+const multerS3 = require("multer-s3");
 
+const storageS3 = new aws.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 
 module.exports = {
-    dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-    storage: multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'))
-        },
-        //so that files with the same name are not received
-        filename: (req, file, cb) => {
-            crypto.randomBytes(16, (err, hash) => {
-                if (err) cb(err);
-
-                const fileName = `${hash.toString('hex')}-${file.originalname}`
-
-                cb(null, fileName);
-            })
-        }
-    }),
-    limits: {
-        fileSize: 2 * 1024 * 1024
+  dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.resolve(__dirname, "..", "..", "tmp", "uploads"));
     },
-    fileFilter: (req, file, cb) => {
-        //only these formats will be received
-        const allowedMimes = [
-            'image/jpeg',
-            'image/pjpeg',
-            'image/png',
-            'image/gif',
-        ]
+    //so that files with the same name are not received
+    filename: (req, file, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err);
 
-        if(allowedMimes.includes(file.mimetype)) {
-            cb(null, true);
-        } else {
-            cb(new Error("Invalid file type."))
-        }
+        const fileName = `${hash.toString("hex")}-${file.originalname}`;
+
+        cb(null, fileName);
+      });
+    },
+  }),
+  limits: {
+    fileSize: 2 * 1024 * 1024,
+  },
+  //only these formats will be received
+  fileFilter: (req, file, cb) => {
+    const allowedMimes = [
+      "image/jpeg",
+      "image/pjpeg",
+      "image/png",
+      "image/gif",
+    ];
+
+    if (allowedMimes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Invalid file type."));
     }
+  },
 };
