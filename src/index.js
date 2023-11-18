eb3b25b6b3d52b1ce0d79f5a0c0f9d818891a17d
Evilis Glenio <glenio.developer@gmail.com>
2023-11-18 09:30:44 -0300

    mongoDB configuration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-const express = require('express');
-const morgan = require('morgan');
+require("dotenv").config();
+
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
 
+/**
+ * Database Setup
+ */
+mongoose.connect("mongodb://localhost:27017/upload", {
+  useNewUrlParser: true,
+});
+
 app.use(express.json()); // so that the express starts giving you the body of messages coming in json format
-app.use(express.urlencoded({ extended: true})); // so that express can give you requests in the urlenconded pattern
-app.use(morgan('dev')); // log lib
+app.use(express.urlencoded({ extended: true })); // so that express can give you requests in the urlenconded pattern
+app.use(morgan("dev")); // log lib
 
 app.use(require("./routes"));
 
